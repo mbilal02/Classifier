732b95281f16e0c9b5457a475770416589b3fbe6
mbilal02 <muhammadbilall@outlook.com>
2019-11-19 23:06:54 +0100

    Bert word Embedding

--- algorithm/model.py
+++ algorithm/model.py
@@ -14,28 +14,6 @@ log= logger.getLoggerClass()
 
 class NER:
 
-
-    def createModel(self):
-        """
-        creating the model form the layers
-        :return:
-        """
-        raise NotImplementedError
-
-    def predict(self):
-        """
-            the method will return the prediction
-        :return:
-        """
-        pass
-
-    def get_evaluation(self):
-        """
-        evaluation script
-        :return:
-        """
-        pass
-
     def get_features(self,list_name, max_seq_length):
 
 
